fd599fe257bfdf716d73cfa8d516e1f5f3ff9572
Michal Frankiewicz <michal.frankiewicz@nordicsemi.no>
2025-01-30 14:03:37 +0100

    applications: sdp: mspi: Fixed bit blocking on bus width change
    Fixed bug where after increasing bus width using buffered registers,
    in first clock cycle the mask for previous bus width was still present.
    
    Signed-off-by: Michal Frankiewicz <michal.frankiewicz@nordicsemi.no>

--- applications/sdp/mspi/src/hrt/hrt.s
+++ applications/sdp/mspi/src/hrt/hrt.s
@@ -8,100 +8,158 @@
 	.align	1
 	.type	hrt_tx, @function
 hrt_tx:
-	addi	sp,sp,-24
-	sw	ra,20(sp)
-	sw	s0,16(sp)
-	sw	s1,12(sp)
-	lw	a4,4(a0)
-	sw	a2,0(sp)
-	sw	a3,4(sp)
-	beq	a4,zero,.L1
-	slli	s0,a1,12
-	li	a4,126976
-	and	s0,s0,a4
+	lw	a5,4(a0)
+	beq	a5,zero,.L16
+	addi	sp,sp,-32
+	sw	s1,20(sp)
 	li	a4,32
-	div	a4,a4,a1
-	mv	a5,a0
+	mv	s1,a1
+	div	a4,a4,s1
+	lui	t1,%hi(xfer_shift_ctrl)
+	sw	s0,24(sp)
+	sw	ra,28(sp)
+	addi	a1,t1,%lo(xfer_shift_ctrl)
+	lbu	t0,2(a1)
+	lbu	a5,1(a1)
+	sb	s1,2(a1)
+	lbu	a1,3(a1)
+	mv	s0,a0
+	slli	a5,a5,8
+	li	a0,3145728
+	slli	a1,a1,20
+	and	a1,a1,a0
+	andi	a5,a5,1792
+	or	a5,a5,a1
+	li	a0,126976
+	slli	a1,s1,12
+	and	a1,a1,a0
+	or	a5,a5,a1
 	addi	a4,a4,-1
+	andi	a4,a4,0xff
+	sb	a4,%lo(xfer_shift_ctrl)(t1)
 	andi	a4,a4,63
-	or	a4,a4,s0
-	ori	a4,a4,1024
+	or	a5,a5,a4
  #APP
-	csrw 3019, a4
+	csrw 3019, a5
  #NO_APP
-	li	s1,0
+	li	a4,2031616
+	slli	a5,s1,16
+	and	a5,a5,a4
+	ori	a5,a5,4
+	sw	a5,0(sp)
+	li	a1,0
 .L3:
-	lw	a4,4(a5)
-	bltu	s1,a4,.L10
-.L1:
-	lw	ra,20(sp)
-	lw	s0,16(sp)
-	lw	s1,12(sp)
-	addi	sp,sp,24
+	lw	a5,4(s0)
+	bltu	a1,a5,.L11
+	lw	ra,28(sp)
+	lw	s0,24(sp)
+	lw	s1,20(sp)
+	addi	sp,sp,32
 	jr	ra
-.L10:
-	lw	a4,4(a5)
-	li	a2,1
-	sub	a4,a4,s1
-	beq	a4,a2,.L4
-	li	a2,2
-	beq	a4,a2,.L5
-.L6:
-	lw	a4,0(a5)
-	bne	a4,zero,.L8
-	lw	a4,16(a5)
-	sw	a5,8(sp)
-	li	a0,0
-	j	.L14
-.L4:
-	lbu	a4,8(a5)
-	addi	a4,a4,-1
-	andi	a4,a4,63
-	or	a4,a4,s0
-	ori	a4,a4,1024
- #APP
-	csrw 3019, a4
- #NO_APP
-	lw	a4,16(a5)
-	lw	a0,12(a5)
-	sw	a5,8(sp)
-.L14:
-	jalr	a4
-.L13:
-	lw	a5,8(sp)
-	bne	s1,zero,.L9
-	lw	a4,0(sp)
-	lbu	a4,0(a4)
-	bne	a4,zero,.L9
-	lw	a4,4(sp)
- #APP
-	csrw 2005, a4
- #NO_APP
-	lw	a3,0(sp)
+.L11:
+	lw	a5,4(s0)
 	li	a4,1
-	sb	a4,0(a3)
+	sub	a5,a5,a1
+	beq	a5,a4,.L4
+	li	a4,2
+	beq	a5,a4,.L5
+.L6:
+	lw	a5,0(s0)
+	li	a0,0
+	beq	a5,zero,.L7
+	lw	a5,0(s0)
+	slli	a4,a1,2
+	add	a5,a5,a4
+	lw	a0,0(a5)
+	j	.L7
+.L4:
+	addi	t2,t1,%lo(xfer_shift_ctrl)
+	lbu	a0,1(t2)
+	lbu	a5,2(t2)
+	li	ra,126976
+	slli	a0,a0,8
+	slli	a5,a5,12
+	andi	a0,a0,1792
+	and	a5,a5,ra
+	lbu	a4,8(s0)
+	or	a5,a0,a5
+	lbu	a0,3(t2)
+	addi	a4,a4,-1
+	li	t2,3145728
+	slli	a0,a0,20
+	andi	a4,a4,0xff
+	and	a0,a0,t2
+	sb	a4,%lo(xfer_shift_ctrl)(t1)
+	or	a5,a5,a0
+	andi	a4,a4,63
+	or	a5,a5,a4
+ #APP
+	csrw 3019, a5
+ #NO_APP
+	lw	a0,12(s0)
+.L7:
+	beq	s1,t0,.L8
 .L9:
-	addi	s1,s1,1
+ #APP
+	csrr a5, 3022
+ #NO_APP
+	andi	a5,a5,0xff
+	bne	a5,zero,.L9
+	lw	a5,0(sp)
+ #APP
+	csrw 3043, a5
+ #NO_APP
+	mv	t0,s1
+.L8:
+	lw	a5,16(s0)
+	sw	a3,16(sp)
+	sw	a2,12(sp)
+	sw	t0,8(sp)
+	sw	a1,4(sp)
+	jalr	a5
+	lw	a1,4(sp)
+	lw	t0,8(sp)
+	lw	a2,12(sp)
+	lw	a3,16(sp)
+	lui	t1,%hi(xfer_shift_ctrl)
+	bne	a1,zero,.L10
+	lbu	a5,0(a2)
+	bne	a5,zero,.L10
+ #APP
+	csrw 2005, a3
+ #NO_APP
+	li	a5,1
+	sb	a5,0(a2)
+.L10:
+	addi	a1,a1,1
 	j	.L3
 .L5:
-	lbu	a4,9(a5)
+	addi	t2,t1,%lo(xfer_shift_ctrl)
+	lbu	a0,1(t2)
+	lbu	a5,2(t2)
+	li	ra,126976
+	slli	a0,a0,8
+	slli	a5,a5,12
+	andi	a0,a0,1792
+	and	a5,a5,ra
+	lbu	a4,9(s0)
+	or	a5,a0,a5
+	lbu	a0,3(t2)
 	addi	a4,a4,-1
+	li	t2,3145728
+	slli	a0,a0,20
+	andi	a4,a4,0xff
+	and	a0,a0,t2
+	sb	a4,%lo(xfer_shift_ctrl)(t1)
+	or	a5,a5,a0
 	andi	a4,a4,63
-	or	a4,a4,s0
-	ori	a4,a4,1024
+	or	a5,a5,a4
  #APP
-	csrw 3019, a4
+	csrw 3019, a5
  #NO_APP
 	j	.L6
-.L8:
-	lw	a2,16(a5)
-	lw	a4,0(a5)
-	slli	a1,s1,2
-	sw	a5,8(sp)
-	add	a4,a4,a1
-	lw	a0,0(a4)
-	jalr	a2
-	j	.L13
+.L16:
+	ret
 	.size	hrt_tx, .-hrt_tx
 	.section	.text.hrt_write,"ax",@progbits
 	.align	1
@@ -120,23 +178,25 @@ hrt_write:
 	li	a5,0
 	addi	a4,a0,4
 	li	a3,4
-.L17:
+.L21:
 	lw	a2,0(a4)
-	bne	a2,zero,.L16
+	bne	a2,zero,.L20
 	addi	a5,a5,1
 	andi	a5,a5,0xff
 	addi	a4,a4,20
-	bne	a5,a3,.L17
+	bne	a5,a3,.L21
 	li	a5,3
-.L16:
-	li	a4,1
-	beq	a5,a4,.L18
-	li	a4,3
-	beq	a5,a4,.L19
-	li	a4,0
-	bne	a5,zero,.L20
-	lbu	a4,80(s0)
 .L20:
+	li	a4,1
+	beq	a5,a4,.L22
+	li	a4,3
+	beq	a5,a4,.L23
+	li	a4,0
+	bne	a5,zero,.L24
+	lbu	a4,80(s0)
+.L24:
+	lui	a3,%hi(xfer_shift_ctrl+2)
+	sb	a4,%lo(xfer_shift_ctrl+2)(a3)
  #APP
 	csrw 2000, 2
  #NO_APP
@@ -163,21 +223,21 @@ hrt_write:
 	li	a2,1
 	add	a5,s0,a5
 	lw	a3,4(a5)
-	beq	a3,a2,.L21
+	beq	a3,a2,.L25
 	li	a2,2
-	beq	a3,a2,.L22
+	beq	a3,a2,.L26
 	li	a5,32
 	div	a5,a5,a4
-	j	.L39
-.L18:
+	j	.L43
+.L22:
 	lbu	a4,81(s0)
-	j	.L20
-.L19:
+	j	.L24
+.L23:
 	lbu	a4,83(s0)
-	j	.L20
-.L21:
+	j	.L24
+.L25:
 	lbu	a5,8(a5)
-.L39:
+.L43:
  #APP
 	csrw 3022, a5
  #NO_APP
@@ -187,11 +247,11 @@ hrt_write:
 	lbu	a4,88(s0)
 	slli	a5,a5,16
 	srli	a5,a5,16
-	bne	a4,zero,.L25
+	bne	a4,zero,.L29
  #APP
 	csrc 3008, a5
  #NO_APP
-.L26:
+.L30:
 	lhu	a3,84(s0)
 	lbu	a1,80(s0)
 	addi	a2,sp,3
@@ -213,17 +273,17 @@ hrt_write:
 	addi	a0,s0,60
 	call	hrt_tx
 	lbu	a5,89(s0)
-	beq	a5,zero,.L27
-.L28:
+	beq	a5,zero,.L31
+.L32:
  #APP
 	csrr a5, 3022
  #NO_APP
 	andi	a5,a5,0xff
-	bne	a5,zero,.L28
+	bne	a5,zero,.L32
  #APP
 	csrw 2010, 0
  #NO_APP
-.L27:
+.L31:
 	li	a5,16384
 	addi	a5,a5,1
  #APP
@@ -232,33 +292,42 @@ hrt_write:
 	csrw 2000, 0
  #NO_APP
 	lbu	a5,87(s0)
-	bne	a5,zero,.L15
+	bne	a5,zero,.L19
 	lbu	a4,86(s0)
 	li	a5,1
 	sll	a5,a5,a4
 	lbu	a4,88(s0)
 	slli	a5,a5,16
 	srli	a5,a5,16
-	bne	a4,zero,.L30
+	bne	a4,zero,.L34
  #APP
 	csrs 3008, a5
  #NO_APP
-.L15:
+.L19:
 	lw	ra,12(sp)
 	lw	s0,8(sp)
 	addi	sp,sp,16
 	jr	ra
-.L22:
+.L26:
 	lbu	a5,9(a5)
-	j	.L39
-.L25:
+	j	.L43
+.L29:
  #APP
 	csrs 3008, a5
  #NO_APP
-	j	.L26
-.L30:
+	j	.L30
+.L34:
  #APP
 	csrc 3008, a5
  #NO_APP
-	j	.L15
+	j	.L19
 	.size	hrt_write, .-hrt_write
+	.section	.sdata.xfer_shift_ctrl,"aw"
+	.align	2
+	.type	xfer_shift_ctrl, @object
+	.size	xfer_shift_ctrl, 4
+xfer_shift_ctrl:
+	.byte	31
+	.byte	4
+	.byte	1
+	.byte	0
